8fa5c616ee3174a964d849b7056fae9cf84ddd2e
bruceouyang <bruceouyang@tencent.com>
2022-08-24 15:36:27 +0800

    feat: add hooks test

--- src/__test__/hook.test.ts
+++ src/__test__/hook.test.ts
@@ -1,1 +1,56 @@
-// todo
+import { act, renderHook } from '@testing-library/react';
+import { createFrexContext } from '../context';
+import { createFrexHook } from '../hook';
+
+function getFrexHook(options: { keepAlive?: boolean } = {}) {
+  const context = createFrexContext({ initState: { count: 1 }, ...options });
+  return { useFrexHook: createFrexHook(context), context };
+}
+
+describe('use hook', () => {
+  it('one components', () => {
+    const { useFrexHook } = getFrexHook();
+    const { result } = renderHook(useFrexHook);
+    expect(result.current[0]).toMatchObject({ count: 1 });
+
+    act(() => {
+      result.current[1]({ count: 3 });
+    });
+
+    expect(result.current[0]).toMatchObject({ count: 3 });
+  });
+
+  it('more components', async () => {
+    const { useFrexHook } = getFrexHook();
+    const { result } = renderHook(useFrexHook);
+    const { result: result2 } = renderHook(useFrexHook);
+
+    expect(result.current[0]).toMatchObject({ count: 1 });
+    expect(result2.current[0]).toMatchObject({ count: 1 });
+
+    act(() => {
+      result.current[1]({ count: 10 });
+    });
+
+    expect(result.current[0]).toMatchObject({ count: 10 });
+    expect(result2.current[0]).toMatchObject({ count: 10 });
+  });
+
+  it('with countext', () => {
+    const { useFrexHook, context } = getFrexHook();
+    const { result } = renderHook(useFrexHook);
+    expect(result.current[0]).toMatchObject({ count: 1 });
+
+    act(() => {
+      result.current[1]({ count: 10 });
+    });
+
+    expect(result.current[0]).toMatchObject({ count: 10 });
+
+    act(() => {
+      context.setState({ count: 20 });
+    });
+
+    expect(result.current[0]).toMatchObject({ count: 20 });
+  });
+});
