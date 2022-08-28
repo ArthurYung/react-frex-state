0458af6abe461ca9bc025ed2e6470af0b6529384
bruceouyang <bruceouyang@tencent.com>
2022-08-28 14:55:19 +0800

    chore: update basurl

--- .umirc.ts
+++ .umirc.ts
@@ -7,5 +7,8 @@ export default defineConfig({
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   mode: 'doc',
+  base: '/react-frex-state/',
+  publicPath: '/react-frex-state/',
+  exportStatic: {},
   // more config: https://d.umijs.org/config
 });
